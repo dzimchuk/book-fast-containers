93c2ea138ba5f30ee19a46e681b086042a34a6b4
Andrei Dzimchuk <andrei@dzimchuk.net>
2019-11-15 14:24:49 +0300

    added Search service

---
+++ BookFast.Search/Program.cs
@@ -0,0 +1,29 @@
+using BookFast.Configuration;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace BookFast.Search
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    if (context.HostingEnvironment.IsProduction())
+                    {
+                        config.AddAzureKeyVault();
+                    }
+                })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}

a82abb4ff21b227fc0844582a9900287b8e159c2
Andrei Dzimchuk <3907708+dzimchuk@users.noreply.github.com>
2026-08-21 00:03:54 +0300

    Update to OpenIddict v7

--- src/Services/Identity/BookFast.Identity.Infrastructure/Database/Migrations/20260820204249_OpenIddict_v7.Designer.cs
+++ src/Services/Identity/BookFast.Identity.Infrastructure/Database/Migrations/20260820204249_OpenIddict_v7.Designer.cs
@@ -4,6 +4,7 @@ using BookFast.Identity.Infrastructure.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BookFast.Identity.Infrastructure.Database.Migrations
 {
     [DbContext(typeof(IdentityContext))]
-    partial class IdentityContextModelSnapshot : ModelSnapshot
+    [Migration("20260820204249_OpenIddict_v7")]
+    partial class OpenIddict_v7
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Services/Identity/BookFast.Identity.Infrastructure/Database/Migrations/20260820204249_OpenIddict_v7.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookFast.Identity.Infrastructure.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class OpenIddict_v7 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "type",
+                schema: "identity",
+                table: "openiddict_tokens",
+                type: "nvarchar(150)",
+                maxLength: 150,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "type",
+                schema: "identity",
+                table: "openiddict_tokens",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(150)",
+                oldMaxLength: 150,
+                oldNullable: true);
+        }
+    }
+}
